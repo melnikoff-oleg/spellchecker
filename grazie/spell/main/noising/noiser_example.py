5563d2f5d96f95559615d29446135bebe170157d
Oleg Melnikov <olegmelnikov@Olegs-MacBook-Pro.local>
2021-10-31 23:54:25 +0300

    fix noiser_example

--- grazie/spell/main/noising/noiser_example.py
+++ grazie/spell/main/noising/noiser_example.py
@@ -1,17 +1,21 @@
-from .noiser import WordReplacementNoiser
-from .noiser import CharacterReplacementNoiser
-from .noiser import ProbabilisticCharacterReplacementNoiser
+from grazie.spell.main.noising.noiser import WordReplacementNoiser
+from grazie.spell.main.noising.noiser import CharacterReplacementNoiser
+from grazie.spell.main.noising.noiser import ProbabilisticCharacterReplacementNoiser
 
-example_texts = [
-    "This is an example sentence to demonstrate noising in the neuspell repository.",
-    "Here is another such amazing example !!",
-    "Sasha Khvorov is my scientific director",
-    "Now I'm writing some code"
-]
+def main():
+    example_texts = [
+        "This is an example sentence to demonstrate noising in the neuspell repository.",
+        "Here is another such amazing example !!",
+        "Sasha Khvorov is my scientific director",
+        "Now I'm writing some code"
+    ]
 
-# word_repl_noiser = WordReplacementNoiser(language="english")
-# word_repl_noiser = CharacterReplacementNoiser(language="english")
-word_repl_noiser = ProbabilisticCharacterReplacementNoiser(language="english")
-word_repl_noiser.load_resources()
-noise_texts = word_repl_noiser.noise(example_texts)
-print(noise_texts)
+    # word_repl_noiser = WordReplacementNoiser(language="english")
+    # word_repl_noiser = CharacterReplacementNoiser(language="english")
+    word_repl_noiser = ProbabilisticCharacterReplacementNoiser(language="english")
+    word_repl_noiser.load_resources()
+    noise_texts = word_repl_noiser.noise(example_texts)
+    print(noise_texts)
+
+if __name__ == '__main__':
+    main()
