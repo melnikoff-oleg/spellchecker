d85ac4c7f466156f23899a64294223ecfec63a15
Oleg Melnikov <olegmelnikov@Olegs-MacBook-Pro.local>
2021-11-01 00:17:33 +0300

    syntetic_data_generation fixes requested in PR

--- grazie/spell/main/noising/__init__.py
+++ grazie/spell/main/noising/__init__.py
@@ -1,6 +1,6 @@
-from .noiser import CharacterReplacementNoiser
-from .noiser import ProbabilisticCharacterReplacementNoiser
-from .noiser import WordReplacementNoiser
+from grazie.spell.main.noising.noiser import CharacterReplacementNoiser
+from grazie.spell.main.noising.noiser import ProbabilisticCharacterReplacementNoiser
+from grazie.spell.main.noising.noiser import WordReplacementNoiser
 
 __all__ = [
     "CharacterReplacementNoiser",
