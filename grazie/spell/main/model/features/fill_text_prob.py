6888369a50b344c23215d7c493742f227c4329c3
akhvorov <khvorov.aleksandr@gmail.com>
2021-11-11 18:18:10 +0300

    Refactor fill text probability

--- grazie/spell/main/model/features/fill_text_prob.py
+++ grazie/spell/main/model/features/fill_text_prob.py
@@ -14,49 +14,40 @@ class FillTextProbComputer:
     def create_prompt(self, text: str, start: int, end: int) -> str:
         return text[:start] + '<mask>' + text[end:]
 
-    def log_probs(self, texts_with_ranges: List[Tuple[str, int, int]], candidates: List[List[str]]) -> List[List[float]]:
-        texts = []
+    def preprocess(self, texts_with_ranges: List[Tuple[str, int, int]], candidates: List[List[str]]) -> Tuple[
+        List[int], List[Tuple[int, int]], List[str]]:
         outs = []
         texts_inds = []
         cands_ranges = []
-        all_candidates = []
         for i, ((text, start, finish), cands) in enumerate(zip(texts_with_ranges, candidates)):
             text_start = text[:start]
             # remove if it is not separate phrase (space or start_of_text in the begin and end_of_text or not alpha after phrase
             if (start == 0 or text[start - 1] == ' ') and (finish == len(text) or not text[finish].isalpha()):
-                all_candidates += cands
                 texts_inds += [i for _ in cands]
-
-                input_text = self.create_prompt(text, start, finish)
-                texts += [input_text for _ in cands]
 
                 output_texts = [text_start + syn + text[finish:] for syn in cands]
                 outs += output_texts
 
-                cands_ranges += [(len(self.tokenizer.encode(text_start[:-1])) - 1, len(self.tokenizer.encode(syn, add_special_tokens=False))) for syn in cands]
+                start_tokens_len = len(self.tokenizer.encode(text_start[:-1])) - 1
+                cands_ranges += [(start_tokens_len, len(self.tokenizer.encode(syn, add_special_tokens=False))) for syn
+                                 in cands]
+        return texts_inds, cands_ranges, outs
 
-        encoded_input = self.tokenizer.batch_encode_plus(
+    def encode_prompt(self, texts):
+        return self.tokenizer.batch_encode_plus(
             texts,
             add_special_tokens=True,
             return_tensors='pt',
             return_attention_mask=True,
             truncation=True, padding=True
-        ).to(self.device)['input_ids']
-        encoded_output = self.tokenizer.batch_encode_plus(
-            outs,
-            add_special_tokens=True,
-            return_tensors='pt',
-            return_attention_mask=True,
-            truncation=True, padding=True
-        ).to(self.device)['input_ids']
+        ).to(self.device)
 
-        all_logits = self.model(encoded_input, decoder_input_ids=encoded_output).logits.cpu()
-
-        scores: Dict[int, List[float]] = {}
+    @staticmethod
+    def candidate_scores(texts_inds: List[int], cands_ranges: List[Tuple[int, int]],
+                         encoded_output: torch.Tensor, all_logits: torch.Tensor) -> Dict[int, List[float]]:
+        scores: Dict[int, List[float]] = {i: [] for i in set(texts_inds)}
         for i, logits in enumerate(all_logits):
             ind = texts_inds[i]
-            if ind not in scores:
-                scores[ind] = []
 
             syn_range = cands_ranges[i]
             word_logits = logits[syn_range[0] - 1:syn_range[0] + syn_range[1] - 1]
@@ -66,8 +57,17 @@ class FillTextProbComputer:
                 word_log_prob += log_probs[j, token_idx]
 
             scores[ind].append(word_log_prob.item())
+        return scores
 
-        result: List[List[float]] = [[] for _ in texts_with_ranges]
-        for i in scores:
-            result[i] = scores[i]
-        return result
+    def log_probs(self, texts_with_ranges: List[Tuple[str, int, int]], candidates: List[List[str]]) -> List[List[float]]:
+        texts_inds, cands_ranges, outs = self.preprocess(texts_with_ranges, candidates)
+        input_prompts = [self.create_prompt(*texts_with_ranges[i]) for i in texts_inds]
+
+        encoded_input = self.encode_prompt(input_prompts)
+        output_ids = self.encode_prompt(outs)['input_ids']
+
+        all_logits = self.model(encoded_input['input_ids'], attention_mask=encoded_input['attention_mask'],
+                                decoder_input_ids=output_ids).logits.cpu()
+
+        scores = self.candidate_scores(texts_inds, cands_ranges, output_ids, all_logits)
+        return list(scores.values())
