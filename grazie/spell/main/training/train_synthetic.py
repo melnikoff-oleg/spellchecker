575fc9bbc36b6ce9eb6538e675b143619bbedcbf
olegmelnikov <oleghools@gmail.com>
2021-12-03 02:48:13 +0300

    added big exp with all mistakes

--- grazie/spell/main/training/train_synthetic.py
+++ grazie/spell/main/training/train_synthetic.py
@@ -154,9 +154,9 @@ def main():
     bigrams_table_path = '/Users/olegmelnikov/PycharmProjects/jb-spellchecker/grazie/spell/main/data/n_gram_freqs/2_grams.csv'
     trigrams_table_path = '/Users/olegmelnikov/PycharmProjects/jb-spellchecker/grazie/spell/main/data/n_gram_freqs/3_grams.csv'
     # model_save_path = '/Users/olegmelnikov/Downloads/ranker_model'
-    experiment_save_path = '/Users/olegmelnikov/PycharmProjects/jb-spellchecker/grazie/spell/main/data/experiments/best_model_full_detail.json'
+    experiment_save_path = '/Users/olegmelnikov/PycharmProjects/jb-spellchecker/grazie/spell/main/data/experiments/best_model_full_detail2.json'
     dataset_name = gt_texts_path.split('/')[-1]
-    train_data, test_data = get_test_data(gt_texts_path, noise_texts_path, size=500)
+    train_data, test_data = get_test_data(gt_texts_path, noise_texts_path)
 
     detectors = [HunspellDetector(), DictionaryDetector()]
     candidators = [HunspellCandidator(), LevenshteinCandidator(max_err=2, index_prefix_len=2)]
@@ -169,7 +169,9 @@ def main():
     ranker = CatBoostRanker(iterations=100)
     ranker_features = [
         # ['bart_prob', 'bert_prob'],
-        ['bigram_freq', 'levenshtein']
+        ["bigram_freq", "trigram_freq", "cand_length_diff",
+         "init_word_length", "levenshtein", "freq", "soundex",
+         "metaphone", "keyboard_dist", "cands_less_dist"]
     ]
     for rf in ranker_features:
         train_model(detector, candidator, ranker, rf, train_data, test_data, freqs_table_path, bigrams_table_path,
